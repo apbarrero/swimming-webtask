a71ebe83c393a3b1fd03dd9feceda8614ad81070
Antonio Pérez Barrero <apbarrero@gmail.com>
2016-02-01 00:58:48 +0100

    Extract meters and seconds from tweet text using regexs

--- swimming.js
+++ swimming.js
@@ -14,14 +14,30 @@ var insertSwimming = function(db, date, meters, seconds, cb) {
    });
 };
 
+var getMeters = function(text) {
+   var re = /(\d+) meters/;
+   var match = re.exec(text);
+   return match[1];
+};
+
+var getSeconds = function(text) {
+   var re = /(\d+)'(\d{2})/;
+   var match = re.exec(text);
+   return (match[1]*60) + parseInt(match[2], 10);
+};
+
 module.exports = function (ctx, done) {
    var dbUri = ctx.data.MONGO_URL;
+   var date = new Date(ctx.data.createdAt);
+   var meters = getMeters(ctx.data.text);
+   var seconds = getSeconds(ctx.data.text);
+
+   console.log(ctx.data.createdAt);
+   console.log(ctx.data.text);
+
    MongoClient.connect(dbUri, function (err, db) {
       if(err) done(err);
 
-      var date = ctx.data.date;
-      var meters = ctx.data.meters;
-      var seconds = ctx.data.seconds;
 
       insertSwimming(db, date, meters, seconds, function() {
          db.close();
